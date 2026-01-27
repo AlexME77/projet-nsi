e86919980140a91a424223c54f6389d995b3da50
mkl0808 <mickaelmaiemery@gmail.com>
2026-01-27 10:32:28 +0100

    Ajout gps.py et position.py

---
+++ gps.py
@@ -0,0 +1,43 @@
+import math
+
+def convertir_ddmm(valeur, orientation):
+    valeur = float(valeur)
+    degres = int(valeur // 100)
+    minutes = valeur - degres * 100
+    coord = degres + minutes / 60
+    if orientation in ['S', 'W']:
+        coord = -coord
+    return coord
+
+
+def extraire_position_GPRMC(trame):
+    champs = trame.split(",")
+    if champs[2] != "A":
+        return None
+    latitude = convertir_ddmm(champs[3], champs[4])
+    longitude = convertir_ddmm(champs[5], champs[6])
+    return latitude, longitude
+
+
+def calculer_orientation_voulue(position, destination):
+    """
+    Calcule l'orientation (en degrés) à suivre pour aller
+    de la position actuelle vers la destination
+    """
+    lat1, lon1 = position
+    lat2, lon2 = destination
+    delta_lat = lat2 - lat1
+    delta_lon = lon2 - lon1
+    angle_rad = math.atan2(delta_lon, delta_lat)
+    angle_deg = math.degrees(angle_rad)
+    if angle_deg < 0:
+        angle_deg += 360
+    return angle_deg
+
+def calculer_correction(orientation_actuelle, orientation_voulue):
+    angle = orientation_voulue - orientation_actuelle
+    if angle > 180:
+        angle -= 360
+    elif angle < -180:
+        angle += 360
+    return angle
---
+++ position.py
@@ -0,0 +1,34 @@
+import serial
+from gps import (extraire_position_GPRMC, calculer_orientation_voulue, calculer_correction)
+#gps = serial.Serial("COM4", 4800, timeout=1)
+#while True:
+#    trame = gps.readline().decode("ascii", errors="ignore").strip()
+#    if trame.startswith("$GPRMC"):
+#        position = extraire_position_GPRMC(trame)
+#        if position is not None:
+#            print("Position actuelle :", position)
+            
+position = (0.0, 0.0)
+destination = (1.0, 0.0)
+print(calculer_orientation_voulue(position, destination))
+destination = (0.0, 1.0)
+print(calculer_orientation_voulue(position, destination))
+destination = (-1.0, 0.0)
+print(calculer_orientation_voulue(position, destination))
+destination = (0.0, -1.0)
+print(calculer_orientation_voulue(position, destination))
+
+orientation_actuelle = 0
+orientation_voulue = 90
+print(calculer_correction(orientation_actuelle, orientation_voulue))
+orientation_actuelle = 90
+orientation_voulue = 0
+print(calculer_correction(orientation_actuelle, orientation_voulue))
+orientation_actuelle = 350
+orientation_voulue = 10
+print(calculer_correction(orientation_actuelle, orientation_voulue))
+
+position = (48.88294, 2.61215) # position GPS réelle
+destination = (48.88350, 2.61300) # destination fictive
+angle_voulu = calculer_orientation_voulue(position, destination)
+print("Orientation à suivre :", angle_voulu)
