d1d4e6f5f64e0c127de1d2ce747aa5169fb94041
AlexME77 <alexandremaiemery@gmail.com>
2026-02-03 11:16:13 +0100

    code non fonctionnel : implémentation de la distance avec capteur ultrason

--- alex/main.py
+++ alex/main.py
@@ -6,8 +6,18 @@ def main():
     robot.set_settings(40)
 
     while True:
-        robot.avant()
-        time.sleep(1)
+        distance = robot.distance_obstacle()
+        if distance < 20:
+            robot.arret()
+            time.sleep(1)
+            robot.recule()
+            time.sleep(1)
+            robot.droite()
+            time.sleep(1)
+        else:
+            robot.avant()
+            time.sleep(1)
+        time.sleep(0.1)
 
 if __name__ == "__main__":
     main()
