47bd89cb83f4b5c8a4d55c2a2ed850b3b891c996
AlexME77 <alexandremaiemery@gmail.com>
2026-02-03 23:21:10 +0100

    correction sans robot du programme main avec mise en place du try except pour propre + tests pour capteurs ultrasons

--- alex/main.py
+++ alex/main.py
@@ -1,23 +1,38 @@
 from robot import Robot
 import time
+import RPi.GPIO as GPIO
+GPIO.setwarnings(False)
 
 def main():
     robot = Robot()
     robot.set_settings(40)
 
-    while True:
-        distance = robot.distance_obstacle()
-        if distance < 20:
-            robot.arret()
-            time.sleep(1)
-            robot.recule()
-            time.sleep(1)
-            robot.droite()
-            time.sleep(1)
-        else:
-            robot.avant()
-            time.sleep(1)
-        time.sleep(0.1)
+    try:
+        while True:
+            distance = robot.distance_obstacle()
+            if distance < 20:
+                robot.arret()
+                time.sleep(1)
+                robot.arriere()
+                time.sleep(1)
+                robot.droite()
+                time.sleep(1)
+            else:
+                robot.avant()
+                time.sleep(1)
+            time.sleep(0.1)
+    
+    except KeyboardInterrupt:
+        print("Arrêt du programme par l'utilisateur")
+    
+    finally:
+        robot.arret()
+        GPIO.cleanup()
+        print("GPIO nettoyé et arrêt du robot")
 
 if __name__ == "__main__":
-    main()
+#    main()
+    while True:
+        test = Robot()
+        print(test.distance_obstacle())
+        time.sleep(1)
