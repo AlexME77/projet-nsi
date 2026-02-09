4be6af45f089750cb22e7cdfb78ba0b1b8b2f11f
AlexME77 <alexandremaiemery@gmail.com>
2026-02-09 14:29:23 +0100

    Ajout du code de navigation et changement du main_alex (pas fonctionnel)

--- controle/navigation_robot.py
+++ controle/navigation_robot.py
@@ -1,7 +1,48 @@
 import time
 
 distance_arrivee=1.5
-distance_obstacle=10
+distance_min=10
 seuil_angle=5
 
-def navigation(robot, get_angle_cible, get_distance_cible, )
+def eviter_obstacle(robot):
+    robot.arret()
+    time.sleep(1)
+    robot.arriere()
+    time.sleep(1)
+    robot.droite()
+    time.sleep(1)
+    robot.avant()
+    time.sleep(1)
+
+def navigation(robot, get_angle_cible, get_distance_cible, get_angle_actuel):
+    while True:
+        
+        distance_obstacle = robot.distance_obstacle()
+        if distance_obstacle < distance_min:
+            eviter_obstacle(robot)
+            continue
+
+        distance_cible = get_distance_cible()
+        if distance_cible < distance_arrivee:
+            robot.stop()
+            print("Arrivé à destination")
+            break
+
+        angle_cible = get_angle_cible()
+        angle_actuel = get_angle_actuel()
+        angle_diff = (angle_cible - angle_actuel)
+        if angle_diff > 180:
+            angle_diff -= 360
+        elif angle_diff < -180:
+            angle_diff += 360
+
+        if abs(angle_diff) < seuil_angle:
+            robot.avant()
+        elif abs(angle_diff) > 0:
+            robot.droite(min(angle_diff, 30))
+            time.sleep(0.2)
+        elif abs(angle_diff) < 0:
+            robot.gauche()
+            time.sleep(0.2)
+        time.sleep(0.1)
+
--- main_alex.py
+++ main_alex.py
@@ -1,7 +1,21 @@
 from robot import Robot
-import time
+from controle.navigation_robot import navigation
 import RPi.GPIO as GPIO
 GPIO.setwarnings(False)
+from gps.gps import GPS
+import time
+
+
+angle_actuel = 0
+distance_cible = 10
+
+def get_angle_cible():
+    return 90
+
+def get_distance_cible():
+    global distance_cible
+    distance_cible -= 0.1
+    return distance_cible
 
 def eviter_obstacle(robot):
     robot.arret()
@@ -13,7 +27,7 @@ def eviter_obstacle(robot):
     robot.avant()
     time.sleep(1)
 
-def main():
+def main_temporaire():
     robot = Robot()
     distance_seuil=10
     vitesse=40
@@ -38,5 +52,20 @@ def main():
         GPIO.cleanup()
         print("GPIO nettoyé et arrêt du robot")
 
+def main():
+    robot = Robot()
+    robot.set_settings(40)
+
+    try:
+        navigation(robot, get_angle_cible, get_distance_cible, angle_actuel)
+
+    except KeyboardInterrupt:
+        print("Arrêt du programme par l'utilisateur")
+    
+    finally:
+        robot.arret()
+        GPIO.cleanup()
+        print("GPIO nettoyé et arrêt du robot")
+
 if __name__ == "__main__":
-    main()
+    main()  
