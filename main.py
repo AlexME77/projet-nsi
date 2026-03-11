dec60dcc9c847829dced6e6134cd8a2f698f53c8
AlexME77 <alexandremaiemery@gmail.com>
2026-03-11 13:11:04 +0100

    fin de séance du 11/03

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ GPIO.setwarnings(False)
 import serial
 import serial.tools.list_ports
 from gps.gps import GPS
-from gps.database import lire_destination
+from gps.database import coord_destination
 import time
 
 def main(nom_parcours):
@@ -14,10 +14,9 @@ def main(nom_parcours):
     robot.set_settings(50)
 
     #recupération du parcours depuis la base de données
-    points_parcours = coord_destination(nom_parcours)
 
     try:
-        navigation(robot, points_parcours)
+        navigation(robot, nom_parcours=nom_parcours)
 
     except KeyboardInterrupt:
         print("Arrêt du programme par l'utilisateur")
