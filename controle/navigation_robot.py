f5f94571960d92755a85f97ae44d253f588fc100
mkl0808 <mickaelmaiemery@gmail.com>
2026-03-18 12:28:26 +0100

    Corrections séance 18/03

--- controle/navigation_robot.py
+++ controle/navigation_robot.py
@@ -36,11 +36,14 @@ def navigation(robot, points):
     seuil_obstacle = 20
     i=0
     fin = False
+    orientation_robot = gps.angle_depart()
 
-    while not fin: 
+    while not fin:
+        
+        position = gps.get_position_robot()
 
         "Récupération de la distance à la cible"
-        distance_arrivee = gps.get_distance_cible(points, ordre=i)
+        distance_arrivee = gps.distance2pGPS(position, points[i])
 
         print("Vérification de la distance à la cible")
         if distance_arrivee < seuil_arrivee:
@@ -57,26 +60,33 @@ def navigation(robot, points):
                 return
 
         print("Calcul de l'orientation")
-        orientation = gps.correction_orientation(points, i)
+        orientation_voulue = gps.get_orientation(position, points[i])
+        correction = orientation_voulue - orientation_robot
+        if correction > 180:
+            correction -= 360
+        if correction < -180:
+            correction += 360
 
         print("Correction de l'orientation")
-        if orientation > -5 and orientation < 5:
+        if -5 < correction < 5:
+            robot.avant()
+            time.sleep(1)
             continue
-        elif orientation < 0:
+        elif correction < 0:
                 robot.rotation_trigo()
         else:
             robot.rotation_horaire()    
-        time.sleep(abs(orientation/30))  # Ajuster le temps de rotation en fonction de l'orientation (trouver la bonne valeur et faire attention à la vitesse garder toujours la même)
+        time.sleep(abs(correction/30))  # Ajuster le temps de rotation en fonction de l'orientation (trouver la bonne valeur et faire attention à la vitesse garder toujours la même)
 # On peut même faire une fonction ou une relation qui calcule le temps de rotation en fonction de l'orientation et de la vitesse du robot
+
+        orientation_robot += correction
+        orientation_robot = orientation_robot % 360
 
         print("Vérification de la présence d'obstacles")
         if robot.distance_obstacle() < seuil_obstacle:
-            
-            print("Obstacle détecté, calcul de la  direction d'évitement")
-            orientation = gps.correction_orientation(points, i)
 
             print("Évitement de l'obstacle")
-            eviter_obstacle(robot, direction="droite" if orientation > 0 else "gauche")
+            eviter_obstacle(robot, direction="droite" if correction > 0 else "gauche")
 
         else:
             print("Avance vers la cible ")
